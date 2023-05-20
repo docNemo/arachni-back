eba22ea5f8a8fef5f515e63fd869cc551a411419
docNemo <varyag300300@yandex.ru>
2023-05-20 21:43:59 +0300

    963545: fix duplicate categories in request

--- src/main/java/ru/mai/arachni/service/ArticleService.java
+++ src/main/java/ru/mai/arachni/service/ArticleService.java
@@ -14,6 +14,7 @@ import ru.mai.arachni.repository.ArticleRepository;
 import ru.mai.arachni.repository.CategoryRepository;
 import ru.mai.arachni.repository.CreatorRepository;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -37,10 +38,9 @@ public class ArticleService {
         }
     }
 
-    @Transactional(readOnly = true)
     void setCategoriesToArticle(final Article article, final List<String> categories) {
         article.setCategories(
-                categories
+                new HashSet<>(categories)
                         .stream()
                         .map(this::chooseCategory)
                         .collect(Collectors.toList())
